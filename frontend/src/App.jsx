25915e87affa683beaa1447fc7589a8ae2ccd539
ADITYA JHA <a4adityajha@gmail.com>
2023-12-15 17:02:18 +0530

    created file

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+// import { ClassNames } from "@emotion/react"
+import React from "react"
+
+function App() {
+  
+
+  return (
+    <>       
+    <div className="bg-red text-9xl "> Hello !</div>
+    </>
+  )
+}
+
+export default App
