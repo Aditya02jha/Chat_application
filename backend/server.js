5ccf5671188242b288efae1ebdcc916e8edef2cc
ADITYA JHA <a4adityajha@gmail.com>
2023-12-17 02:25:36 +0530

    full working of signup and login page

--- backend/server.js
+++ backend/server.js
@@ -5,13 +5,15 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const AuthRoutes = require('./Routes/auth')
 const messagesroute = require('./Routes/messagesroutes')
+require ("dotenv").config();
+const socket = require("socket.io");
 
 const server = express();
 server.use(cors());
 server.use(express.json());
 server.use(express.urlencoded({extended: true}));
 
-mongoose.connect('mongodb://localhost:27017' , {
+mongoose.connect(`mongodb://localhost:27017/chat_application` , {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => {
@@ -22,5 +24,28 @@ mongoose.connect('mongodb://localhost:27017' , {
 server.use('/api/auth' , AuthRoutes);
 // server.use('/api/messages' , messagesroute);
 
-const PORT = (3000 || process.env.PORT);
+const PORT = (5000 );
 server.listen(PORT , console.log(`server is running on ${PORT}`))
+
+// const io = socket(server, {
+//     cors: {
+//       origin: "http://localhost:3000",
+//       credentials: true,
+//     },
+//   });
+  
+//   global.onlineUsers = new Map();
+//   io.on("connection", (socket) => {
+//     global.chatSocket = socket;
+//     socket.on("add-user", (userId) => {
+//       onlineUsers.set(userId, socket.id);
+//     });
+  
+//     socket.on("send-msg", (data) => {
+//       const sendUserSocket = onlineUsers.get(data.to);
+//       if (sendUserSocket) {
+//         socket.to(sendUserSocket).emit("msg-recieve", data.msg);
+//       }
+//     });
+//   });
+
--- frontend/src/components/Login_page.jsx
+++ frontend/src/components/Login_page.jsx
@@ -1,29 +1,81 @@
-import { useEffect, useState, useRef } from "react";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import styled from "styled-components";
+import { useNavigate, Link } from "react-router-dom";
+// import Logo from "../assets/logo.svg";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { loginRoute } from "../utils/APIRoutes";
 
-
-const Login_page = () => {
-  const [value, setValue] = useState({
-    username: "",
-    password: "",
-  });
-
-  const usernameInputRef = useRef(null);
+export default function Login() {
+  const navigate = useNavigate();
+  const [values, setValues] = useState({ username: "", password: "" });
+  const toastOptions = {
+    position: "bottom-right",
+    autoClose: 8000,
+    pauseOnHover: true,
+    draggable: true,
+    theme: "dark",
+  };
   useEffect(() => {
-    usernameInputRef.current.focus();
+    if (localStorage.getItem("chat-app-current-use")) {
+      navigate("/");
+    }
   }, []);
 
-  function handleChange(e) {
-    setValue({ ...value, [e.target.name]: e.target.value });
-  }
+  const handleChange = (event) => {
+    setValues({ ...values, [event.target.name]: event.target.value });
+  };
 
-  function handleSubmit(e) {
-    e.preventDefault();
-  }
+  const validateForm = () => {
+    const { username, password } = values;
+    if (username === "") {
+      toast.error("Email and Password is required.", toastOptions);
+      return false;
+    } else if (password === "") {
+      toast.error("Email and Password is required.", toastOptions);
+      return false;
+    }
+    return true;
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (validateForm()) {
+      const { username, password } = values;
+      const { data } = await axios.post(loginRoute, {
+        username,
+        password,
+      });
+      if (data.status === false) {
+        toast.error(data.msg, toastOptions);
+      }
+      if (data.status === true) {
+        localStorage.setItem(
+          "chat-app-current-user",
+          JSON.stringify(data.user)
+        );
+
+        navigate("/");
+      }
+    }
+  };
 
   return (
     <div className="flex items-center justify-center h-screen bg-gradient-to-br from-magenta-500 to-blue-500">
       <div className="bg-white p-8 rounded-md shadow-md w-96">
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
         <form onSubmit={handleSubmit}>
           <label className="block text-gray-700">UserName</label>
           <input
@@ -31,15 +83,15 @@ const Login_page = () => {
             name="username"
             required
             onChange={handleChange}
-            ref={usernameInputRef}
             className="mt-1 p-2 w-full border rounded-md"
           />
 
           <label className="block text-gray-700 mt-4">Password</label>
           <input
-            onChange={handleChange}
             type="password"
             name="password"
+            required
+            onChange={handleChange}
             className="mt-1 p-2 w-full border rounded-md"
           />
           <button
@@ -49,9 +101,77 @@ const Login_page = () => {
             LOGIN
           </button>
         </form>
+        <ToastContainer />
       </div>
     </div>
   );
-};
+}
 
-export default Login_page;
+// const FormContainer = styled.div`
+//   height: 100vh;
+//   width: 100vw;
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: center;
+//   gap: 1rem;
+//   align-items: center;
+//   background-color: #131324;
+//   .brand {
+//     display: flex;
+//     align-items: center;
+//     gap: 1rem;
+//     justify-content: center;
+//     img {
+//       height: 5rem;
+//     }
+//     h1 {
+//       color: white;
+//       text-transform: uppercase;
+//     }
+//   }
+
+//   form {
+//     display: flex;
+//     flex-direction: column;
+//     gap: 2rem;
+//     background-color: #00000076;
+//     border-radius: 2rem;
+//     padding: 5rem;
+//   }
+//   input {
+//     background-color: transparent;
+//     padding: 1rem;
+//     border: 0.1rem solid #4e0eff;
+//     border-radius: 0.4rem;
+//     color: white;
+//     width: 100%;
+//     font-size: 1rem;
+//     &:focus {
+//       border: 0.1rem solid #997af0;
+//       outline: none;
+//     }
+//   }
+//   button {
+//     background-color: #4e0eff;
+//     color: white;
+//     padding: 1rem 2rem;
+//     border: none;
+//     font-weight: bold;
+//     cursor: pointer;
+//     border-radius: 0.4rem;
+//     font-size: 1rem;
+//     text-transform: uppercase;
+//     &:hover {
+//       background-color: #4e0eff;
+//     }
+//   }
+//   span {
+//     color: white;
+//     text-transform: uppercase;
+//     a {
+//       color: #4e0eff;
+//       text-decoration: none;
+//       font-weight: bold;
+//     }
+//   }
+// `;
